81dbab5a25e68c7eb806815a7f1ec3d3beb184ae
N <Ranjith.N@unisys.com>
2023-08-30 04:03:09 -0700

    Test

---
+++ OpenEMRAutomation/LogiUITest.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.OpenEMRAutomation
+{
+    public class LogiUITest
+    {
+        [Test]
+        public void ValidateTitleTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://demo.openemr.io/b/openemr";
+            string actualTitle=driver.Title;
+            Assert.AreEqual("OpenEMR Login",actualTitle);
+        }
+    }
+}
